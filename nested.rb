e33fcc463a58e27d678d70b573b542c9b3e497c1
Abdul Khan <akhan17@outlook.com>
2020-02-16 18:19:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,7 +106,6 @@ def changing_alan
      alans_new_info = "GUI"
      
     programmer_hash[:alan_kay][:known_for] = alans_new_info
-    return programmer_hash[:alan_kay][:known_for]
      
 end
 
